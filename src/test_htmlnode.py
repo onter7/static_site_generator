ac3d7c61a07b3c5e682dee47b199231184d3b673
onter7 <markovigor191@gmail.com>
2024-05-19 23:40:20 +0300

    add tests for HTMLNode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,10 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        props = {"href": "https://www.google.com", "target": "_blank"}
+        node = HTMLNode(None, None, None, props)
+        self.assertEqual("href=\"https://www.google.com\" target=\"_blank\"", node.props_to_html())
