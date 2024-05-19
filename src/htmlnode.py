ff96393f447181a6e33cd1af8135a5c528c699fd
onter7 <markovigor191@gmail.com>
2024-05-20 00:15:24 +0300

    moved LeafNode to htmlnode.py and unit tests to test_htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,8 +12,19 @@ class HTMLNode:
     def props_to_html(self):
         if not self.props:
             return ""
-
         return " " + " ".join([f"{key}=\"{value}\"" for key, value in self.props.items()])
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("No value in the leaf node")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -8,3 +8,19 @@ class TestHTMLNode(unittest.TestCase):
         props = {"href": "https://www.google.com", "target": "_blank"}
         node = HTMLNode(None, None, None, props)
         self.assertEqual(" href=\"https://www.google.com\" target=\"_blank\"", node.props_to_html())
+
+    def test_to_html_paragraph(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual("<p>This is a paragraph of text.</p>", node.to_html())
+
+    def test_to_html_anchor_with_href(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual("<a href=\"https://www.google.com\">Click me!</a>", node.to_html())
+
+    def test_no_tag(self):
+        node = LeafNode(None, "It's just a simple text.")
+        self.assertEqual("It's just a simple text.", node.to_html())
+
+    def test_no_value(self):
+        node = LeafNode()
+        self.assertRaises(ValueError, node.to_html)
