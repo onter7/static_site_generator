e18230e2f07c0137eb0c96f190163021059ee25c
onter7 <markovigor191@gmail.com>
2024-05-20 00:04:44 +0300

    fix unit test for HTMLNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,4 +7,4 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         props = {"href": "https://www.google.com", "target": "_blank"}
         node = HTMLNode(None, None, None, props)
-        self.assertEqual("href=\"https://www.google.com\" target=\"_blank\"", node.props_to_html())
+        self.assertEqual(" href=\"https://www.google.com\" target=\"_blank\"", node.props_to_html())
