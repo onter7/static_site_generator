a29faae6dbe96762f32f88b658fc11519f3cbe5f
onter7 <markovigor191@gmail.com>
2024-05-30 21:57:20 +0300

    add block_to_block_type function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,29 @@
+import re
+
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if bool(re.match(r"^[#]{1,6} ", block)):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    if lines and all([line.startswith(">") for line in lines]):
+        return block_type_quote
+    if lines and (all([line.startswith("* ") for line in lines]) or all([line.startswith("- ") for line in lines])):
+        return block_type_unordered_list
+    if lines and all([line.startswith(f"{idx+1}. ") for idx, line in enumerate(block.split("\n"))]):
+        return block_type_ordered_list
+    return block_type_paragraph
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
