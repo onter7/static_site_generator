2f50c5ab927035b41cc7948abdb3cc020e13adaf
onter7 <markovigor191@gmail.com>
2024-05-30 21:57:45 +0300

    add tests for block_to_block_type function

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -47,6 +47,43 @@ This is the same paragraph on a new line
             blocks
         )
 
+    def test_block_to_block_type_heading(self):
+        block = "### Heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+
+    def test_block_to_block_type_code(self):
+        block = "```code```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+
+    def test_block_to_block_type_quote(self):
+        block = """> Quote start
+> Quote
+> Quote end"""
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+
+    def test_block_to_block_type_unordered_list_asteriscs(self):
+        block = """* One
+* Two
+* Three"""
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+
+    def test_block_to_block_type_unordered_list_dashes(self):
+        block = """- One
+- Two
+- Three"""
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. One
+2. Two
+3. Three"""
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+
+    def test_block_to_block_type_paragraph(self):
+        block = """Just a simple
+text"""
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+
 
 if __name__ == "__main__":
     unittest.main()
