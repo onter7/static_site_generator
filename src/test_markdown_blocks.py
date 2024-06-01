06ea0bfa76f348697f2602ea7711b0ad984147f3
onter7 <markovigor191@gmail.com>
2024-06-01 15:10:36 +0300

    add tests for block_to_html_node function

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -84,6 +84,82 @@ This is the same paragraph on a new line
 text"""
         self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>"
+        )
+
+    def test_headings(self):
+        md = """
+# Heading with **bolded** text
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>Heading with <b>bolded</b> text</h1></div>"
+        )
+
+    def test_quote(self):
+        md = """
+>This is a quote
+>with *italic* text
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a quote with <i>italic</i> text</blockquote></div>"
+        )
+
+    def test_code(self):
+        md = """
+```
+This is a code block
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is a code block</code></pre></div>"
+        )
+
+    def test_unordered_list(self):
+        md = """
+- Item 1
+- Item 2
+- Item 3 *(additional)*
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>Item 1</li><li>Item 2</li><li>Item 3 <i>(additional)</i></li></ul></div>"
+        )
+
+    def test_ordered_list(self):
+        md = """
+1. Item 1
+2. Item 2
+3. Item 3 **important**
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>Item 1</li><li>Item 2</li><li>Item 3 <b>important</b></li></ol></div>"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
