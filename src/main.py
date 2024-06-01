f7bddb7576d5b5ce360f748aff4a465463c66126
onter7 <markovigor191@gmail.com>
2024-06-02 00:34:15 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copystatic import copy_content
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    copy_content(dir_path_static, dir_path_public)
 
 
 main()
