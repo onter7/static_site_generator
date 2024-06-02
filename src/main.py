f0ec423edd2cf995efb68e2adabb012c0d98f325
onter7 <markovigor191@gmail.com>
2024-06-02 11:24:54 +0300

    update main file and run script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
 import shutil
 
+
 from copystatic import copy_content
+from getcontent import generate_page
 
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+path_content = "./content/index.md"
+path_template = "./template.html"
+path_index = "./public/index.html"
 
 
 def main():
@@ -13,6 +18,7 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_content(dir_path_static, dir_path_public)
+    generate_page(path_content, path_template, path_index)
 
 
 main()
