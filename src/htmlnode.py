cbf6778778f7c9029acc348c3d6694747b40ac7a
onter7 <markovigor191@gmail.com>
2024-06-02 11:22:36 +0300

    fixed htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,9 +26,9 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("Invalid HTML: not value")
-        if not self.tag:
+        if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
@@ -41,9 +41,9 @@ class ParentNode(HTMLNode):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if not self.tag:
+        if self.tag is None:
             raise ValueError("Invalid HTML: no tag")
-        if not self.children:
+        if self.children is None:
             raise ValueError("Invalid HTML: no children")
         html = f"<{self.tag}>"
         for child in self.children:
